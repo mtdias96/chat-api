79cdd938369a254af5b51e0d9755311f8c9b750c
mtdias96 <matheusdias.front96@gmail.com>
2025-03-03 11:17:27 -0300

    feat: authenticateRepository/add methods create and findUnique

---
+++ src/shared/database/repositories/auth/authenticate.repositories.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { type Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma.service';
+
+@Injectable()
+export class AuthenticateRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createDto: Prisma.UserCreateInput) {
+    return this.prismaService.user.create({ data: createDto });
+  }
+
+  findUnique(findUniqueDto: Prisma.UserFindUniqueArgs) {
+    return this.prismaService.user.findUnique(findUniqueDto);
+  }
+}
